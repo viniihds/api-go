6bcb8757de33eda83ba9b71e4c8a26461d1220e4
viniihds <viniciushsilva2210@gmail.com>
2023-08-12 21:31:14 -0300

    Add Logger

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/viniihds/api-go/router"
+import (
+
+	"github.com/viniihds/api-go/router"
+	"github.com/viniihds/api-go/config"
+)
+
+var(
+	logger *config.Logger
+)
 
 func main() {
 	//Inialização Router
 	router.Initialize()
+
+
+	logger = config.GetLogger("main")
+	//Inialização Configs
+	err := config.Init()
+	if err != nil{
+		logger.Errf("Erro na Inicialização da Config: %v", err)
+		return
+	}
 }
